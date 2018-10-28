7fb6d0999b5f1381e3f84e6fbf3bfd31b417de45
Amal <amal.alabdulkarim@gmail.com>
2018-10-28 15:09:03 -0400

    inital commit

---
+++ GradientOptimizations.py
@@ -0,0 +1,24 @@
+class GradientOptimizations(object):
+    def __init__(self):
+        pass
+
+    def GD(self):
+        pass
+
+    def SGD(self):
+        pass
+
+    def SGDM(self):
+        pass
+
+    def adaGrad(self):
+        pass
+
+    def RMSProp(self):
+        pass
+
+    def AdaDelta(self):
+        pass
+
+    def ADAM(self):
+        pass
